3b2c19c92de1ede5d08d3cd5a1d100f84a95d24c
Brandon Keefer <happysolucki@outlook.com>
2021-08-18 11:52:20 -0500

    added map to favorites page

--- js/favorites.js
+++ js/favorites.js
@@ -3,6 +3,31 @@ let potentialGallery = JSON.parse(localStorage.getItem("gallery")) || allParks;
 let parkGallery = new Gallery(potentialGallery, potentialFavs);
 parkGallery.renderFavorites();
 
+const locations = parkGallery.favorites.map((item) => item.coordinates);
+// console.log(testmap);
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 3,
+    center: { lat: 39.83, lng: -95.58 },
+  });
+  // Create an array of alphabetical characters used to label the markers.
+  const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  // Add some markers to the map.
+  // Note: The code uses the JavaScript Array.prototype.map() method to
+  // create an array of markers based on a given "locations" array.
+  // The map() method here has nothing to do with the Google Maps API.
+  const markers = locations.map((location, i) => {
+    return new google.maps.Marker({
+      position: location,
+      label: labels[i % labels.length],
+    });
+  });
+  // Add a marker clusterer to manage the markers.
+  new MarkerClusterer(map, markers, {
+    imagePath: "./assets/markers/m",
+  });
+}
+
 function handleUnFavClick(evt) {
   console.log(evt.target.id);
   if (evt.target.id === "unfav") {
@@ -13,8 +38,22 @@ function handleUnFavClick(evt) {
     evt.target.id = "fav";
     evt.target.textContent = "Fav";
   }
-  
+
   parkGallery.renderFavorites();
 }
+
+// const handleUnfavorite = (evt) => {
+//   console.log(evt.target.id);
+//   if (evt.target.id === "unfav") {
+//     let currentPark = evt.target.parentElement;
+//     let currentParkName = currentPark.querySelector("#name").textContent;
+//     console.log(currentParkName);
+//     parkGallery.removeFavorite(currentParkName);
+//     evt.target.id = "fav";
+//     evt.target.textContent = "Fav";
+//   }
+
+//   parkGallery.renderFavorites();
+// };
 
 favsList.addEventListener("click", handleUnFavClick);
